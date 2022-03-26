3157e1aa64950720b8b98b0f6984b7c541d23a8f
Alysson Victor <alyssonsantos@alunos.utfpr.edu.br>
2022-03-25 22:08:13 -0300

    Add base file for profile page

---
+++ lib/profile_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ProfilePageState();
+  }
+}
+
+class ProfilePageState extends State<ProfilePage> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
