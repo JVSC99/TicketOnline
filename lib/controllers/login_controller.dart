60c2c2e746de5e3c48f5c50c6a131dda42a3960e
JVSC99 <jvsc76@gmail.com>
2022-03-31 23:21:55 -0300

    Versão funcionando mas sem validação no login

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,13 +1,1 @@
-import 'dart:js';
-
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:ticket_online/pages/home_page.dart';
-
-class LoginController {
-  LoginController(BuildContext context) {
-    Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return HomePage();
-    }));
-  }
-}
+class LoginController {}
