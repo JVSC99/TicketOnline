bde8017ab5161fe1df088b2960d7866e87182121
Alysson Victor <alysson@pop-os.localdomain>
2022-05-10 22:43:15 -0300

    [DB] Add helper database class

---
+++ lib/database/db.dart
@@ -0,0 +1,60 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DB {
+  DB._();
+
+  static final DB instance = DB._();
+
+  static Database? _database;
+
+  get database async {
+    if(_database != null){
+      return _database;
+    }
+
+    return await _initDatabase();
+  } 
+
+  _initDatabase() async {
+    return await openDatabase(
+      join(await getDatabasesPath(), "ticket.db"),
+      version: 1,
+      onCreate: _onCreate,
+
+    );
+  }
+
+  _onCreate(db, versao) async {
+    await db.execute(_aluno);
+    await db.execute(_carteira);
+    await db.execute(_historico);
+  }
+
+  String get _aluno => '''
+    CREATE TABLE aluno (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      nome TEXT,
+      ra TEXT,
+      senha TEXT,
+      carteira_id INTEGER,
+      historico_id INTEGER,
+      imagePath TEXT
+    )
+  ''';  
+
+  String get _carteira => '''
+    CREATE TABLE carteira (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      saldo REAL,
+    )
+  ''';  
+
+  String get _historico => '''
+    CREATE TABLE historico (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      data TEXT,
+      refeicao TEXT
+    )
+  ''';  
+}
