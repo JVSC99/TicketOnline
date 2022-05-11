3b053fd2413d9d5fc535d88603622cd6d1892249
Alysson Victor <alysson@pop-os.localdomain>
2022-05-10 23:03:36 -0300

    [DB] Add alunoId on historico

--- lib/model/historico.dart
+++ lib/model/historico.dart
@@ -1,10 +1,12 @@
 class Historico {
   int id;
+  int alunoId;
   String data;
   String refeicao;
 
   Historico({
     required this.id,
+    required this.alunoId,
     required this.data,
     required this.refeicao,
   });
