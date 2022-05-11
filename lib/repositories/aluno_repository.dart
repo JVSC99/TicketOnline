73b254e24ca634bc7d94e692870742a7257b517c
Alysson Victor <alysson@pop-os.localdomain>
2022-05-10 23:21:01 -0300

    [DB] Add Carteira and Historico repositories

--- lib/repositories/aluno_repository.dart
+++ lib/repositories/aluno_repository.dart
@@ -1,40 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:ticket_online/database/db.dart';
 import 'package:ticket_online/model/aluno.dart';
 import 'package:ticket_online/model/carteira.dart';
 import 'package:ticket_online/model/historico.dart';
+import 'package:ticket_online/repositories/historico_repository.dart';
+import 'package:ticket_online/repositories/carteira_repository.dart';
 
-class AlunoRepository {
+class AlunoRepository extends ChangeNotifier {
+
+  late Database db;
+
+  Aluno? aluno;
+
+  HistoricoRepository historicoRepository = HistoricoRepository();
+
+  CarteiraRepository carteiraRepository = CarteiraRepository();
+
+  findById (int id) async {
+      db = await DB.instance.database;
+      List alunos = await db.query('aluno', where: 'id = $id');
+
+      return null;
+  }
+
+  findByRA (String ra) async {
+      db = await DB.instance.database;
+      List aluno = await db.query('aluno', where: 'ra = $ra');
+      return aluno.first;
+  }
+
+  get () async {
+      db = await DB.instance.database;
+      List alunos = await db.query('aluno');
+      return alunos;
+  }
+
   static List<Aluno> lista = [
     Aluno(
+        id: 1,
         nome: 'João Victor',
         ra: '123',
         senha: '1234',
-        carteira: Carteira(id: 'João_123', saldo: 1),
+        carteira: Carteira(id: 1, saldo: 1),
         historico: [
-          Historico(data: '9/03/2022', refeicao: 'Jantar'),
-          Historico(data: '9/03/2022', refeicao: 'Almoço'),
-          Historico(data: '10/03/2022', refeicao: 'Jantar'),
-          Historico(data: '10/03/2022', refeicao: 'Almoço'),
-          Historico(data: '11/03/2022', refeicao: 'Jantar'),
-          Historico(data: '11/03/2022', refeicao: 'Almoço'),
-          Historico(data: '12/03/2022', refeicao: 'Jantar'),
-          Historico(data: '12/03/2022', refeicao: 'Almoço'),
+          Historico(id: 1, data: '9/03/2022', refeicao: 'Jantar'),
+          Historico(id: 2, data: '9/03/2022', refeicao: 'Almoço'),
+          Historico(id: 3, data: '10/03/2022', refeicao: 'Jantar'),
+          Historico(id: 4, data: '10/03/2022', refeicao: 'Almoço'),
+          Historico(id: 5, data: '11/03/2022', refeicao: 'Jantar'),
+          Historico(id: 6, data: '11/03/2022', refeicao: 'Almoço'),
+          Historico(id: 7, data: '12/03/2022', refeicao: 'Jantar'),
+          Historico(id: 8, data: '12/03/2022', refeicao: 'Almoço'),
         ],
         imagePath:
             "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"),
     Aluno(
+        id: 1,
         nome: 'Alysson Victor',
         ra: '234567',
         senha: '1234',
-        carteira: Carteira(id: 'Alysson_123', saldo: 100),
+        carteira: Carteira(id:1, saldo: 100),
         historico: [
-          Historico(data: '12/03/2022', refeicao: 'Jantar'),
-          Historico(data: '12/03/2022', refeicao: 'Almoço'),
-          Historico(data: '11/03/2022', refeicao: 'Jantar'),
-          Historico(data: '11/03/2022', refeicao: 'Almoço'),
-          Historico(data: '10/03/2022', refeicao: 'Jantar'),
-          Historico(data: '10/03/2022', refeicao: 'Almoço'),
-          Historico(data: '09/03/2022', refeicao: 'Jantar'),
-          Historico(data: '09/03/2022', refeicao: 'Almoço'),
+          Historico(id: 9, data: '12/03/2022', refeicao: 'Jantar'),
+          Historico(id: 10, data: '12/03/2022', refeicao: 'Almoço'),
+          Historico(id: 11, data: '11/03/2022', refeicao: 'Jantar'),
+          Historico(id: 12, data: '11/03/2022', refeicao: 'Almoço'),
+          Historico(id: 13, data: '10/03/2022', refeicao: 'Jantar'),
+          Historico(id: 14, data: '10/03/2022', refeicao: 'Almoço'),
+          Historico(id: 15, data: '09/03/2022', refeicao: 'Jantar'),
+          Historico(id: 16, data: '09/03/2022', refeicao: 'Almoço'),
         ],
         imagePath:
             "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png")
---
+++ lib/repositories/carteira_repository.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:ticket_online/model/carteira.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:ticket_online/database/db.dart';
+
+class CarteiraRepository extends ChangeNotifier {
+
+  late Database db;
+
+  List<Carteira> carteiras = [];
+
+  findById (int id) async {
+
+      db = await DB.instance.database;
+
+      List carteira = await db.query('carteira', where: 'id = $id');
+       
+      if(carteira.isNotEmpty){
+        return Carteira(
+          id: carteira[0]['id'],
+          alunoId: carteira[0]['alunoId'],
+          saldo: carteira[0]['saldo']
+        );
+      }
+
+      return null;
+  }
+
+  getByAlunoId (int id) async {
+      db = await DB.instance.database;
+
+      List carteira = await db.query('carteira', where: 'id = $id');
+       
+      if(carteira.isNotEmpty){
+        return Carteira(
+          id: carteira[0]['id'],
+          alunoId: carteira[0]['alunoId'],
+          saldo: carteira[0]['saldo']
+        );
+      }
+
+      return null;
+  }
+}
