466b2c3938d3c85447aaf35f95bfc51a6094fc0e
Alysson Victor <alysson@pop-os.localdomain>
2022-05-12 20:04:21 -0300

    [DB] Add a controller to LoginPage

---
+++ lib/controller/login_controller.dart
@@ -0,0 +1,24 @@
+import 'package:ticket_online/model/aluno.dart';
+import 'package:ticket_online/repositories/aluno_repository.dart';
+
+class LoginController {
+  
+  AlunoRepository alunoRepository = AlunoRepository();
+
+  Future<Aluno> validarLogin(String ra, String senha) async {
+    
+    try {
+      Aluno aluno = await alunoRepository.findByRA(ra);
+
+      if(aluno.senha == senha){
+        return aluno;
+      }
+
+    } catch (error) {
+      print(error.toString());
+    }
+
+    throw("Senha ou RA inválido");
+  }
+  
+}
